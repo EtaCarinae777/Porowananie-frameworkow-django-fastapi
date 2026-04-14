2e2f94f9c26a1ff21e77e8fcbe6b0054e8c99728
kaczpr <bojarski.kacper02@gmail.com>
2026-04-14 17:53:34 +0200

    Stronka konta połączona z backendem

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,16 +1,21 @@
 import { useNavigate } from 'react-router-dom'
 
 function Account() {
+    const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    navigate('/login'); // Przekierowanie na stronę logowania
+  };
   const navigate = useNavigate()
-
+  const savedUser = JSON.parse(localStorage.getItem('user'));
   return (
     <div style={{ padding: '20px' }}>
       <h2>Moje konto</h2>
       <div style={{ background: 'white', padding: '20px', borderRadius: '8px', marginTop: '10px', maxWidth: '400px' }}>
-        <p><strong>Imię:</strong> Jan</p>
-        <p style={{ marginTop: '8px' }}><strong>Nazwisko:</strong> Kowalski</p>
-        <p style={{ marginTop: '8px' }}><strong>Email:</strong> jan@example.com</p>
-        <button onClick={() => navigate('/')} style={{ marginTop: '20px', background: '#e24a4a' }}>
+        <p><strong>Imię:</strong> {savedUser.first_name}</p>
+        <p style={{ marginTop: '8px' }}><strong>Nazwisko:</strong> {savedUser.last_name}</p>
+        <p style={{ marginTop: '8px' }}><strong>Email:</strong> {savedUser.email}</p>
+        <button onClick={handleLogout} style={{ marginTop: '20px', background: '#e24a4a' }}>
           Wyloguj się
         </button>
       </div>
